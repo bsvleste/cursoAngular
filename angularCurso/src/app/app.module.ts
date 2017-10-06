a96eddeedc1f03b7d824d4661d25435790df6297
Bruno de Souza Valeiro <bvaleiro85@hotmail.com>
2017-10-05 22:09:32 -0300

    aula service

--- angularCurso/src/app/app.module.ts
+++ angularCurso/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import{CursosModule} from './cursos/cursos.module';
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import {CursosService} from './cursos/cursos.service';
+
 @NgModule({
   declarations: [
     AppComponent,
